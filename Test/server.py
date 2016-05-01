3b61774b355715b67fde383978a5a9d9aec65005
Iman Shafiei <imanshafiei@ymail.com>
2016-05-01 18:47:53 +0430

    SiTE Done

--- Test/server.py
+++ Test/server.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, url_for, redirect, session, flash
+
 from functools import wraps
 import sqlite3
+from flask import *
+from flask_login import *
+
 
 
 app = Flask(__name__)
@@ -21,16 +24,77 @@ def login_required(f):
             return redirect(url_for('login'))
     return wrap
 
-
 @app.route('/')
-def home():
+def index():
     return render_template('main.html')
 
-
-@app.route('/welcome')
+@app.route('/welcome', methods=['GET', 'POST'])
 @login_required
 def welcome():
-    return render_template('welcome.html')
+    conn = sqlite3.connect('test.db')
+    if request.method == 'POST':
+        cap = request.form['post']
+        file = request.files['image']
+        print cap
+        print file
+        conn.execute("insert into POST (CAPTION, IMAGE) values (?, ?);",(cap, file))
+        conn.commit()
+        conn.close()
+
+
+    conn = sqlite3.connect('test.db')
+    print "Opened database successfully"
+    cursor = conn.execute("SELECT id, caption, image  from POST")
+    dic = cursor.fetchall()
+    return render_template('hello.html',DATA = dic)
+
+@app.route('/post/<number>',  methods=['GET', 'POST'])
+@login_required
+def post(number):
+    conn = sqlite3.connect('test.db')
+    if request.method == 'POST':
+        print 11111111111
+        COMMENT = request.form['comment']
+        print COMMENT, number
+
+        conn.execute("insert into COMMENT (comment, F_KEY) values (?, ?);",(COMMENT, number))
+        conn.commit()
+        conn.close()
+
+    conn = sqlite3.connect('test.db')
+    cursor = conn.execute("SELECT id, caption, image  from POST")
+    for row in cursor:
+        if str(row[0]) == number.encode('utf-8'):
+
+            id = row[0]
+            caption = row[1]
+            image = row[2]
+
+
+            break
+    cursor2 = conn.execute("SELECT ID, comment, F_KEY from COMMENT")
+    com_dic = {}
+
+    for row in cursor2:
+        if str(row[2]) == number.encode('utf-8'):
+            COM_ID = row[0]
+            COM = row[1]
+            com_dic[str(COM_ID)] = COM.encode('utf-8')
+
+
+
+    cursor3 = conn.execute("SELECT ID, reply, F_KEY_POST,F_KEY_COMMENT from REPLY")
+    rep_dic = {}
+    for row in cursor3:
+
+        if str(row[2]) == number.encode('utf-8') :
+
+            REP = row[1]
+            com_num = row[3]
+            rep_dic[com_num.encode('utf-8')] = REP.encode('utf-8')
+
+
+    return render_template('post.html', ID = id, CAP = caption, IMG = image, COMMENT = com_dic, rep_dic = rep_dic)
 
 
 @app.route('/logout')
@@ -101,8 +165,48 @@ def registration():
     return render_template('signup.html', error=error)
 
 
-def connect_db():
-    return sqlite3.connect(app.database)
+
+
+@app.route('/post/<number>/comment/<number2>', methods=['GET', 'POST'])
+@login_required
+def reply(number,number2):
+    conn = sqlite3.connect('test.db')
+    cursor = conn.execute("SELECT id, caption, image  from POST")
+    for row in cursor:
+        if str(row[0]) == number.encode('utf-8'):
+
+            id = row[0]
+            caption = row[1]
+            image = row[2]
+
+
+            break
+
+    cursor2 = conn.execute("SELECT ID, comment, F_KEY from COMMENT")
+    for row in cursor2:
+        print 111111111111111111111111
+        print str(row[0])
+        print number2.encode('utf-8')
+
+        if str(row[0]) == number2.encode('utf-8'):
+            COM = row[1]
+            print COM
+            break
+        print 111111111111111111111111
+
+
+    cursor3 = conn.execute("SELECT ID, reply, F_KEY_POST,F_KEY_COMMENT from REPLY")
+    rep_dic = {}
+    for row in cursor3:
+
+        if str(row[2]) == number.encode('utf-8') and str(row[3]) == number2.encode('utf-8'):
+
+            REP = row[1]
+            com_num = row[3]
+            rep_dic[com_num.encode('utf-8')] = REP.encode('utf-8')
+
+    return render_template('reply.html',number2 = number2, number = number, rep_dic = rep_dic, ID = id, CAP = caption, IMG = image, com_text = COM)
+
 
 
 if __name__ == '__main__':
